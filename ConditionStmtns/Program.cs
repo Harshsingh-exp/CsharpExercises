4f947e3190ba149c4fc07e1e2b7e7b283e38624f
Harshsingh-exp <65265133+Harshsingh-exp@users.noreply.github.com>
2020-05-12 23:07:16 -0400

    Add files via upload

---
+++ ConditionStmtns/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CSharpExercises.ControlFlow;
+
+class Program {
+    static void Main (string[] args) {
+
+        var obj = new Conditionals ();
+
+        obj.Exercise1 ();
+
+        obj.Exercise2 ();
+
+        obj.Exercise3 ();
+
+        obj.Exercise4 ();
+
+    }
+
+}
